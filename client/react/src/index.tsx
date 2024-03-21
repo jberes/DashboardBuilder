e22c553a8a2a29d4e4d4160745bec08e83b5d8f3
Jason Beres <jasonberes@gmail.com>
2024-03-21 16:21:23 -0400

    Add files via upload

---
+++ client/react/src/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { RevealSdkSettings } from '@revealbi/ui';
+
+RevealSdkSettings.serverUrl =  "https://localhost:51977"
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  //<React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  //</React.StrictMode>
+);
+
+reportWebVitals();

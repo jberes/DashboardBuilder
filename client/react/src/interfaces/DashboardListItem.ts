0d4be0303672fb2445154824ac5c326fa6dfce3f
Jason Beres <jasonberes@gmail.com>
2024-03-21 16:20:30 -0400

    Delete client/react directory

--- client/react/src/interfaces/DashboardListItem.ts
+++
@@ -1,9 +0,0 @@
-export interface DashboardListItem {
-    dashboardFileName: string;
-    dashboardTitle: string;
-    dateCreated: string;
-    dateModified: string;
-    fakeOwner: string;
-    fakeOwnerImageUrl: string;
-    thumbnailInfo?: any;
-  }
